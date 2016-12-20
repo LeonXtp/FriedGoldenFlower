f951f7f411e1a812756f0ff57e393fb4645d7d87
LeonXtp <he.leon.lijun@gmail.com>
2016-12-20 10:20:31 +0800

    readme

--- src/com/leon/fgf/PlayerType.java
+++ src/com/leon/fgf/PlayerType.java
@@ -11,42 +11,42 @@ public class PlayerType {
 	public static final int BOMB = 5;
 	// 最大值AAA=14，加上同花顺6867=7881
 	public static final int BOMB_MAX_VALUE = 7881;
-	// 最小值222=2，加上同花顺6867=7869
-	public static final int BOMB_MIN_VALUE = 7869;
+	// 以上乘64
+	public static final int BOMB_FLOWER_BASE_MAX_VALUE = 504384;
 
 	// 同花顺
 	public static final int STRAIGHT_FLUSH = 4;
 	// 最大值AKQ=12，加上同花7855=7867
 	public static final int STRAIGHT_FLUSH_MAX_VALUE = 7867;
-	// 最小值432=2， 加上同花7855=7857
-	public static final int STRAIGHT_FLUSH_MIN_VALUE = 7857;
+	// 以上乘64
+	public static final int STRAIGHT_FLOWER_FLUSH_BASE_MAX_VALUE = 503448;
 
 	// 同花
 	public static final int FLUSH = 3;
 	// 最大值AKJ，14*16*16+13*16+11=3803，加上顺子4052=7855
 	public static final int FLUSH_MAX_VALUE = 7855;
-	// 最小值532，5*16*16+3*16+2=1330，加上顺子4052=5382
-	public static final int FLUSH_MIN_VALUE = 5382;
+	// 以上乘64
+	public static final int FLUSH_FLOWER_BASE_MAX_VALUE = 502720;
 
 	// 顺子
 	public static final int STRAIGHT = 2;
 	// 最大值AKQ=12，加上对子的最大值基数4040=4052
 	public static final int STRAIGHT_MAX_VALUE = 4052;
-	// 最小值432=2，加上对子的最大基数4040=4042
-	public static final int STRAIGHT_MIN_VALUE = 4042;
+	// 以上乘64
+	public static final int STRAIGHT_FLOWER_BASE_MAX_VALUE = 259328;
 
 	// 对子
 	public static final int DOUBLE = 1;
 	// 最大值AAK=14*16+13=237,加上普通牌的基数3803=4040
 	public static final int DOUBLE_MAX_VALUE = 4040;
-	// 最小值223=2*16+3=35，加上普通牌的基数3803=3838
-	public static final int DOUBLE_MIN_VALUE = 3838;
+	// 以上乘64
+	public static final int DOUBLE_FLOWER_BASE_MAX_VALUE = 258560;
 
 	// 普通牌，里面包含一种特殊牌532不同花色
 	public static final int NORMAL = 0;
 	// 最大值AKJ=14*16*16+13*16+11=3803
 	public static final int NORMAL_MAX_VALUE = 3803;
-	// 最小值532=5*16*16+3*16+2=1330
-	public static final int NORMAL_MIN_VALUE = 1330;
+	// 以上乘64
+	public static final int NORMAL_FLOWER_BASE_MAX_VALUE = 243392;
 
 }
